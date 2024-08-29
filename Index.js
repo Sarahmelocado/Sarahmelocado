f4e1fa06861d09ff50d5054f615520e4325cf59a
Sarahmelocado <ocadosarahmel_bsit@plmun.edu.ph>
2024-08-29 22:46:24 +0800

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const profiles = document.querySelectorAll('.profile img');
+    profiles.forEach(img => {
+        img.addEventListener('mouseover', function() {
+            this.style.width = "120px";
+        });
+        img.addEventListener('mouseout', function() {
+            this.style.width = "100px";
+        });
+    });
+});
